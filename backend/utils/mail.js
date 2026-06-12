8e18912e3ef1632284dfa60e7884655420358509
Amit Shaw <amitwork708090@gmail.com>
2026-06-12 22:53:32 +0530

    Update mail.js

--- backend/utils/mail.js
+++ backend/utils/mail.js
@@ -2,7 +2,7 @@ import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 const transporter = nodemailer.createTransport({
-  host: "smtp.ethereal.email",
+  host: "smtp-relay.brevo.com",
   port: 587,
   secure: false, // true for 465, false for other ports
   auth: {
