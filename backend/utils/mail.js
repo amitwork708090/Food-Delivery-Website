84c9b393d3c7478bc49b77febbd801692324b2d1
Amit Shaw <amitwork708090@gmail.com>
2026-06-12 22:59:02 +0530

    Update mail.js

--- backend/utils/mail.js
+++ backend/utils/mail.js
@@ -2,9 +2,9 @@ import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false, // true for 465, false for other ports
+  service: "Gmail",
+  port: 465,
+  secure: true, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASS,
