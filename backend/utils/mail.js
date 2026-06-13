28f5728b9b0953f5ecc06e5e98232bb21185b277
Amit Shaw <amitwork708090@gmail.com>
2026-06-13 12:10:21 +0530

    Update mail.js

--- backend/utils/mail.js
+++ backend/utils/mail.js
@@ -1,36 +1,34 @@
-import { Resend } from "resend";
+import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  port: 465,
+  secure: true, // true for 465, false for other ports
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
+});
 
-// OTP mail
 export const sendOtpMail = async (to, otp) => {
-  try {
-    await resend.emails.send({
-      from: "onboarding@resend.dev",
-      to,
-      subject: "Reset Your Password",
-      html: `<p>Your OTP is <b>${otp}</b></p>`,
-    });
-  } catch (error) {
-    console.log("Resend Error:", error);
-    throw new Error("Email not sent");
-  }
+  await transporter.sendMail({
+    from: process.env.EMAIL,
+    to,
+    subject: "Reset Your Password",
+    html: `<p>Your OTP for password reset is <b>${otp}</b>. It expires in 5 minutes.</p>`,
+  });
 };
 
-// Delivery OTP
 export const sendDeliveryOtpMail = async (user, otp) => {
-  try {
-    await resend.emails.send({
-      from: "onboarding@resend.dev",
-      to: user.email,
-      subject: "Delivery OTP",
-      html: `<p>Your Delivery OTP is <b>${otp}</b></p>`,
-    });
-  } catch (error) {
-    console.log("Resend Error:", error);
-    throw new Error("Email not sent");
-  }
+  await transporter.sendMail({
+    from: process.env.EMAIL,
+    to: user.email,
+    subject: "Delivery OTP",
+    html: `<p>Your OTP for delivery is <b>${otp}</b>. It expires in 5 minutes.</p>`,
+  });
 };
+
+
