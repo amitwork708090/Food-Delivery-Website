fa56d3f7c94e929ecc70df45e28cc9cf9a316d70
Amit Shaw <amitwork708090@gmail.com>
2026-08-15 00:14:27 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,7 +25,7 @@ import { useEffect } from "react";
 import { io } from "socket.io-client";
 import { setSocket } from "./redux/userSlice";
 
-export const serverUrl = "https://food-delivery-website-beryl-beta.vercel.app";
+export const serverUrl = "https://food-delivery-website-six-psi.vercel.app";
 function App() {
   const { userData } = useSelector((state) => state.user);
   const dispatch = useDispatch();
