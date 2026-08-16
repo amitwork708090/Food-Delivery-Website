ee74e6092c8a7de1d4584d186ff1900634131e24
Amit Shaw <amitwork708090@gmail.com>
2026-08-16 22:11:15 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,37 +8,37 @@ import cors from "cors";
 import itemRouter from "./routes/item.routes.js";
 import shopRouter from "./routes/shop.routes.js";
 import orderRouter from "./routes/order.routes.js";
+import http from "http";
+import { Server } from "socket.io";
+import { socketHandler } from "./socket.js";
+
+import dns from "node:dns";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 dotenv.config();
 
 const app = express();
+const server = http.createServer(app);
 
+const io = new Server(server, {
+  cors: {
+    origin: "https://food-delivery-website-tvkj.vercel.app",
+    credentials: true,
+    methods: ["POST", "GET"],
+  },
+});
+
+app.set("io", io);
+
+const port = process.env.PORT || 5000;
 app.use(
   cors({
     origin: "https://food-delivery-website-tvkj.vercel.app",
     credentials: true,
-  })
+  }),
 );
-
 app.use(express.json());
 app.use(cookieParser());
-
-// MongoDB connection
-app.use(async (req, res, next) => {
-  try {
-    await connectDb();
-    next();
-  } catch (error) {
-    console.error("Database connection failed:", error);
-
-    return res.status(500).json({
-      message: "Database connection failed",
-      error: error.message,
-    });
-  }
-});
-
-// Routes
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/shop", shopRouter);
@@ -49,4 +49,8 @@ app.get("/", (req, res) => {
   res.send("Backend is running");
 });
 
-export default app;
+socketHandler(io);
+server.listen(port, () => {
+  connectDb();
+  console.log(`server started at ${port}`);
+});
