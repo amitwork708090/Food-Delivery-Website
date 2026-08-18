a030c6a0665228cb2191016eaa0f99de883ac25a
Amit Shaw <amitwork708090@gmail.com>
2026-08-18 22:28:30 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,7 +25,7 @@ import { useEffect } from "react";
 import { io } from "socket.io-client";
 import { setSocket } from "./redux/userSlice";
 
-export const serverUrl = "https://food-delivery-website-backend-production-fb54.up.railway.app";
+export const serverUrl = "https://food-delivery-website-backend-vshv.onrender.com";
 function App() {
   const { userData } = useSelector((state) => state.user);
   const dispatch = useDispatch();
